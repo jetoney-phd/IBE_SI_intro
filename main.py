22e6d77136fd083cf742dabb8fe8b22ec07a1ff3
jetoney-phd <94642861+jetoney-phd@users.noreply.github.com>
2024-01-23 15:31:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import GroupA
+import GroupB
+import GroupC
+import GroupD
+import GroupE
+import GroupF
+import GroupG
+import GroupH
+import GroupI
 import TAs
 
 TAs.print_teaching_team()
 GroupA.print_members()
+GroupB.print_members()
+GroupC.print_members()
+GroupD.print_members()
+GroupE.print_members()
+GroupF.print_members()
+GroupG.print_members()
+GroupH.print_members()
+GroupI.print_members()
