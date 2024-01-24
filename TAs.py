da1e831eb76645575e59b5b6a827be82aece656c
jetoney-phd <94642861+jetoney-phd@users.noreply.github.com>
2024-01-24 11:27:26 -0500

    Update TAs.py

--- TAs.py
+++ TAs.py
@@ -1,5 +1,5 @@
 def print_teaching_team():
-  teaching_team = ["Julie Rose", "Richa", ""Mikkel", "Richard"]
+  teaching_team = ["Julie Rose", "Richa", "Mikkel", "Richard"]
 
   for n in teaching_team:
     print(n)
