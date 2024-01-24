6ede900f82ed6caf95fdace04198f410b076d4b1
Jackson Topoleski <146686366+txppxr@users.noreply.github.com>
2024-01-24 11:18:56 -0500

    Update GroupI.py

--- GroupI.py
+++ GroupI.py
@@ -2,5 +2,5 @@ def print_members():
   member_names = ["Jackson Topoleski", "Stephen Adams", "Saket Kolluru", "Eashan Vytla"]
   for n in member_names:
       print(n)
-    print(Group I on top);
+    print("Group I on top")
 
