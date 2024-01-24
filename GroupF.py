5ecdb7b3cf7697a2caa07382314394151e52f272
katieaschmidt4 <152338726+katieaschmidt4@users.noreply.github.com>
2024-01-24 11:21:58 -0500

    Update GroupF.py

--- GroupF.py
+++ GroupF.py
@@ -2,3 +2,4 @@ def print_members():
   member_names = ["Kathleen Schmidt", "Amogh Kuppa", "Rishabh Mehta", "Aidan Hook"]
   for n in member_names:
       print(n)
+  print("This is the best project") 
