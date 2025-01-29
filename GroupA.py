09b5da99bc5d5d604e823ad5392e272776fd12be
jetoney-phd <toney.35@osu.edu>
2025-01-29 15:28:56 -0500

    correct GroupB syntax error

--- GroupA.py
+++ GroupA.py
@@ -1,5 +1,5 @@
 def print_members():
-  member_names = ["Ryan", "Vihaan", "Aidan", "Samara"]
+  member_names = ["Ryan", "Vihaan", "Aidan", "samara"]
   for n in member_names:
       print(n)
   print("Hi")
