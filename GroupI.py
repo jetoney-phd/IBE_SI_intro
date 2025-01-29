1745b95bae83d8430db1ec3db1ebabbb8b0645fe
jetoney-phd <94642861+jetoney-phd@users.noreply.github.com>
2025-01-29 14:27:49 -0500

    Update GroupI.py

--- GroupI.py
+++ GroupI.py
@@ -1,6 +1,8 @@
 def print_members():
   member_names = ["Jackson Topoleski", "Stephen Adams", "Saket Kolluru", "Eashan Vytla"]
   for n in member_names:
-      print(n)
-    print("Group I on top")
-
+    print(n)
+  print("Group I on top")
+    
+if __name__ == "__main__":
+  print_members()
