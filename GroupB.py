f54753b99f8ab1c5f3e2a3c653fb04696f6ae533
jetoney-phd <94642861+jetoney-phd@users.noreply.github.com>
2025-01-29 15:19:22 -0500

    Update GroupB.py

--- GroupB.py
+++ GroupB.py
@@ -2,6 +2,6 @@ def print_members():
   member_names = ["Akshay", "Claire", "Meena", "Ryan"]
   for n in member_names:
       print(n)
-  print("We are awesome :)"
+  print("We are awesome :")
 if __name__ == "__main__":
   print_members()
